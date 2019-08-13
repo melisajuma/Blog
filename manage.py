581f18d1e9b335f49e5411d5e556540892dd4ba1
melisajuma <Melisaakinyi95@gmail.com>
2019-08-13 10:55:29 +0300

    importations

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask_script import Manager,Server
-from app.models import User,Role,Comment,Blog
+from app.model>s import User,Role,Comment,Blog
 from flask_migrate import Migrate,MigrateCommand
 
 
