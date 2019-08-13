b07af1ec6c476c5bb0cece289cda4d2693034fa2
melisajuma <Melisaakinyi95@gmail.com>
2019-08-13 10:23:08 +0300

    setting a function for email validtaion

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField,PasswordField,SubmitField,ValidationError,Boolea
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
-class RegistrationForm>(FlaskForm):
+class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
     username = StringField('Enter your username',validators = [Required()])
     password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
     password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
     submit = SubmitField('Sign Up')
 
-    def validate_email(self,data_field):
+    def validate_email(self,>data_field):
             if User.query.filter_by(email =data_field.data).first():
                 raise ValidationError('There is an account with that email')
 
