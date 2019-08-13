945d88bbb4294c99ffa35878c5489ddd3cc15e08
melisajuma <Melisaakinyi95@gmail.com>
2019-08-13 10:21:33 +0300

    imports

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
-from wtforms.validators import Required,Email,EqualTo
+from wtforms.validators >import Required,Email,EqualTo
 from ..models import User
 
 class RegistrationForm(FlaskForm):
