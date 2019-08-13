7e02882826f569d9e763497d964915c11454e09e
melisajuma <Melisaakinyi95@gmail.com>
2019-08-13 10:57:03 +0300

    creating an app instance

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 from app import create_app, db
 from flask_script import Manager,Server
-from app.model>s import User,Role,Comment,Blog
+from app.models import User,Role,Comment,Blog
 from flask_migrate import Migrate,MigrateCommand
 
 
 
-#Create app instance
+#Create app> instance
 
 app = create_app('production')
 
